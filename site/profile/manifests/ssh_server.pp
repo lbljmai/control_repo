5aed57620421096a24dedde084d9119bb2a96f6f
lbljmai <41753616+lbljmai@users.noreply.github.com>
2020-12-21 11:56:42 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDzBlC+/ef2PU4LnPz8HN9pBTP+7CvYdtsJoPCyskenoo03aC2AumciPBjNy7QGo/VQ0Rvs+MgYf0Nhxyf+hfrpV5BFPKw46HgB2VzNfssWNJjYXqh4212ITQ/m8njqYXsdOBIP2uQOFFYgxzg8Rvy+A2ZEMDafnOm96xpVW/CUDalEXun94zdT3LvtEbCV8NtsVtEn0Z7ebiY1wAFscBsyerg45k5GM4BTU2lUtkqm23CNUhiTVhJpM+gigoyWJC5NLp9/+Xi4nRMrVUV3nGeqUHrg/IF48rabT0687lUZy9R7MsjNMoHXeAEhCm1Ra7abWNyxl/kb4qGMB8stYTqt',
 	}  
 }
